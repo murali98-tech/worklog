9a082e3a796107236a7161ab1a0fadca1392e534
murali krishna <muralikrishna11996@gmail.com>
2021-02-09 08:00:06 +0530

    config server added

--- server/GateWay/src/main/groovy/com/EmployeeLog/GateWay/GateWayApplication.groovy
+++ server/GateWay/src/main/groovy/com/EmployeeLog/GateWay/GateWayApplication.groovy
@@ -2,12 +2,14 @@ package com.EmployeeLog.GateWay
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableConfigServer
 class GateWayApplication {
 
 	static void main(String[] args) {
